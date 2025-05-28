afa6241f3609e1c4c43e33dd6c5c0423d0b08f76
Kyryll Kochkin <kyrylo.kochkin@nure.ua>
2025-05-28 09:29:32 +0200

    implemented /get and /post. /getHash now returns a sha512 hash

--- api.py
+++ api.py
@@ -1,9 +1,11 @@
 import fastapi
+import hashlib
+import datetime
 import os
 
 app = fastapi.FastAPI()
 
-states=[]
+states=dict()
 
 @app.get("/")
 async def hello():
@@ -15,19 +17,20 @@ async def health():
         return fastapi.responses.JSONResponse({"status": "error", "message": "High CPU load"})
     else: return fastapi.responses.JSONResponse({"status": "ok"})
 
-message_buffer = ""
-
 @app.post("/post")
 async def post(data: dict):
-    global message_buffer
-    message_buffer = data.get("message", "No message provided")
-    return fastapi.responses.JSONResponse({"status": "ok"})
+    try:
+        states[data.get("hash")] = data.get("state")
+        return fastapi.responses.JSONResponse({"status": "ok"})
+    except Exception as e:
+        return fastapi.responses.JSONResponse({"status": "error", "message": str(e)}, status_code=400)
 
 @app.get("/get")
-async def get():
-    return fastapi.responses.JSONResponse({"message": message_buffer})
+async def get(hash: str = None):
+    return fastapi.responses.JSONResponse({"state": states.get(hash, "No state found")})
 
 @app.get("/getHash")
 async def get_hash():
-    import hashlib
-    return fastapi.responses.JSONResponse({"hash": f"{hashlib.sha256().hexdigest()}"})
+    tmp = hashlib.sha512().hexdigest()
+    states[tmp] = {None: None}
+    return fastapi.responses.JSONResponse({"hash": f"{tmp}"})
