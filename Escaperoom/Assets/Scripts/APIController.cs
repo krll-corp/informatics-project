144be5e9f175cd6b52d87cd3e5bff20c915cee21
Paul <78820138+MentixLynx@users.noreply.github.com>
2025-05-29 22:25:02 +0200

    added json parsing

--- Escaperoom/Assets/Scripts/APIController.cs
+++ Escaperoom/Assets/Scripts/APIController.cs
@@ -1,15 +1,20 @@
 using System.Collections;
 using UnityEngine;
 using UnityEngine.Networking;
-using static System.Net.WebRequestMethods;
+using Newtonsoft.Json;
 
 public class APIController : MonoBehaviour
 {
+    public class HashData
+    {
+        string hash;
+    }
+
     public string server_url = "https://10.0.20.60:8000/";
 
     void Start()
     {
-        StartCoroutine(getRequest(server_url));
+        StartCoroutine(getHashRequest(server_url));
     }
 
     IEnumerator getRequest(string uri)
@@ -25,6 +30,24 @@ public class APIController : MonoBehaviour
         else
         {
             Debug.Log("Received: " + uwr.downloadHandler.text);
+        }
+    }
+    IEnumerator getHashRequest(string uri)
+    {
+        UnityWebRequest uwr = UnityWebRequest.Get(uri + "getHash");
+        yield return uwr.SendWebRequest();
+
+        if (uwr.responseCode != 200)
+        {
+            Debug.Log("Error While Sending: " + uwr.error);
+            Debug.Log("Response Code: " + uwr.responseCode);
+        }
+        else
+        {
+            Debug.Log("Received: " + uwr.downloadHandler.text);
+
+            HashData data = JsonConvert.DeserializeObject<HashData>(uwr.downloadHandler.text);
+            
         }
     }
 }
