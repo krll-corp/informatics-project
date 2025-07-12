46e9ab064464b30be495849ba0bd8d2a315d1b77
Paul <paul.ferber@gmx.de>
2025-07-12 22:29:53 +0200

    converted to urp

--- Escaperoom/Assets/Scripts/PlayerMovement.cs
+++ Escaperoom/Assets/Scripts/PlayerMovement.cs
@@ -165,6 +165,8 @@ public class PlayerMovement : MonoBehaviour
 
     private void HandleMovement()
     {
+        Debug.Log("Handle Move");
+
         // Calculate movement direction based on player's forward and right vectors
         Vector3 moveDirection = transform.forward * _moveInput.y + transform.right * _moveInput.x;
         moveDirection.Normalize(); // Ensure consistent speed if moving diagonally
